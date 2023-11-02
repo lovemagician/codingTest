4c8bc6544148412c9e45c7205a80fcc642473386
lovemagician <138091757+lovemagician@users.noreply.github.com>
2023-11-03 04:34:52 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,13 +1,3 @@
 function solution(age) {
-    let alphabet = ["a","b","c","d","e","f","g","h","i","j"];
-    let ageNum = age.toString().split("");
-    console.log(ageNum.length);
-    if (ageNum.length === 1){
-        return alphabet[ageNum[0]]
-    } else if (ageNum.length === 2){
-    return alphabet[ageNum[0]] + alphabet[ageNum[1]]
-    } else if (ageNum.length === 3){
-       return alphabet[ageNum[0]] + alphabet[ageNum[1]] + alphabet[ageNum[2]] 
-    } else 
-       return alphabet[ageNum[0]] + alphabet[ageNum[1]] + alphabet[ageNum[2]]  + alphabet[ageNum[3]];
+    return age.toString().split("").map((e) => "abcdefghij"[e]).join("");
 };
