c80a95d70c1426fa05df73a79123b13fe2f5bbfa
lovemagician <138091757+lovemagician@users.noreply.github.com>
2023-11-04 18:07:22 +0900

    [level unrated] Title: 약수 구하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    let result = []
+    for (let i = 0; i <= n; i++) {
+        if (n%i === 0) {
+            result.push(i);
+        }
+        else {
+            continue;
+        }
+    }
+    return result;
+}
