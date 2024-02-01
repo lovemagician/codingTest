36b82d6f46d59ab9dc2bdb54f1338788c2cae4b9
lovemagician <138091757+lovemagician@users.noreply.github.com>
2024-02-01 23:21:41 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,18 +1,12 @@
 function solution(numer1, denom1, numer2, denom2) {
     let denomworld = (denom1 * denom2);
     let numerworld = (denom1 * numer2)+(numer1 * denom2);
-    let gcd = 1
-    
+    let gcd = 0
         for (let i = 1; i <= numerworld; i++) {
         if(denomworld%i === 0 && numerworld%i === 0) {
             gcd = i
         }
-            
     }
-
-    
-    
     let answer = [numerworld/gcd, denomworld/gcd]
-    
     return answer;
 }
